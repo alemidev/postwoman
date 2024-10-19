ee876471a639efcbcc1a988898b159ff15edf171
alemi <me@alemi.dev>
2024-10-19 04:48:08 +0200

    feat: allow running multiple times and in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod model;
 
+use std::sync::Arc;
+
 use clap::{Parser, Subcommand};
 use model::{PostWomanConfig, PostWomanError};
 
@@ -24,6 +26,14 @@ pub enum PostWomanActions {
 	Run {
 		/// regex query filter, run all with '.*'
 		query: String,
+
+		/// run requests in parallel
+		#[arg(long, default_value_t = false)]
+		parallel: bool,
+
+		/// repeat request N times
+		#[arg(long, default_value_t = 1)]
+		repeat: u32,
 	},
 
 	// Save {
@@ -40,6 +50,16 @@ pub enum PostWomanActions {
 	// }
 }
 
+const TIMESTAMP_FMT: &str = "%H:%M:%S%.6f"; 
+
+fn print_results(res: String, name: String, before: chrono::DateTime<chrono::Utc>) {
+	let after = chrono::Utc::now();
+	let elapsed = (after - before).num_milliseconds();
+	let timestamp = after.format(TIMESTAMP_FMT);
+	eprintln!(" + [{timestamp}] {name} done in {elapsed}ms:", );
+	println!("{}", res);
+}
+
 #[tokio::main]
 async fn main() -> Result<(), PostWomanError> {
 	let args = PostWomanArgs::parse();
@@ -48,16 +68,38 @@ async fn main() -> Result<(), PostWomanError> {
 	let config: PostWomanConfig = toml::from_str(&collection)?;
 
 	match args.action {
-		PostWomanActions::Run { query } => {
+		PostWomanActions::Run { query, parallel, repeat } => {
 			let pattern = regex::Regex::new(&query)?;
+			let mut joinset = tokio::task::JoinSet::new();
+			let client = Arc::new(config.client);
 			for (name, endpoint) in config.route {
 				if pattern.find(&name).is_some() {
-					eprintln!("> executing {name}");
-					let res = endpoint
-						.fill()
-						.execute(&config.client)
-						.await?;
-					println!("{res}");
+					for i in 0..repeat {
+						let _client = client.clone();
+						let _endpoint = endpoint.clone();
+						let _name = name.clone();
+						let task = async move {
+							let before = chrono::Utc::now();
+							eprintln!(" : [{}] sending {_name} #{}...", before.format(TIMESTAMP_FMT), i+1);
+							let res = _endpoint
+								.fill()
+								.execute(&_client)
+								.await;
+							(res, _name, before)
+						};
+						if parallel {
+							joinset.spawn(task);
+						} else {
+							let (res, name, before) = task.await;
+							print_results(res?, name, before);
+						}
+					}
+				}
+			}
+			while let Some(j) = joinset.join_next().await {
+				match j {
+					Ok((res, name, before)) => print_results(res?, name, before),
+					Err(e) => eprintln!("! error joining task: {e}"),
 				}
 			}
 		},

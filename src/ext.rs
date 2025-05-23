8845dfe8a2dc4cd2a385f5402aecf4cd6c8afaad
alemi <me@alemi.dev>
2025-05-23 16:59:23 +0200

    feat: generic trait for filling

--- src/ext.rs
+++ src/ext.rs
@@ -58,28 +58,15 @@ pub struct FillError(pub String);
 pub trait FillableFromEnvironment: Sized {
 	fn fill(self, env: &toml::Table) -> Result<Self, FillError>;
 
-	fn replace(mut from: String, env: &HashMap<String, String>) -> Result<String, FillError> {
-		let placeholders: Vec<(String, String)> = var_matcher()
-			.captures_iter(&from)
-			.map(|m| m.extract())
-			.map(|(txt, [var])| (txt.to_string(), var.to_string()))
-			.collect();
-
-		// TODO can we avoid cloning all matches??? can't mutate `from` as captures_iter holds an
-		// immutable reference to original string
-
-		for (txt, var) in placeholders {
-			let value = env.get(&var).ok_or(FillError(var.to_string()))?;
-			from = from.replace(&txt, value);
-		}
-
-		Ok(from)
+	fn replace<T: ReplaceableValue>(from: T, env: &HashMap<String, String>) -> Result<T, FillError> {
+		from.replace_in_self(env)
 	}
 
 	fn default_vars(env: &toml::Table) -> std::collections::HashMap<String, String> {
 		let mut vars: std::collections::HashMap<String, String> = std::collections::HashMap::default();
 
 		vars.insert("POSTWOMAN_TIMESTAMP".to_string(), chrono::Local::now().timestamp().to_string());
+		vars.insert("POSTWOMAN_TODAY".to_string(), chrono::Local::now().format("%d/%m/%Y").to_string());
 		vars.insert("POSTWOMAN_LOCAL_ID".to_string(), uuid::Uuid::new_v4().to_string());
 		vars.insert("POSTWOMAN_SESSION_ID".to_string(), sid().to_string());
 
@@ -92,5 +79,76 @@ pub trait FillableFromEnvironment: Sized {
 		}
 
 		vars
+	}
+}
+
+pub trait ReplaceableValue: Sized {
+	fn replace_in_self(self, env: &HashMap<String, String>) -> Result<Self, FillError>;
+}
+
+impl ReplaceableValue for String {
+	fn replace_in_self(mut self, env: &HashMap<String, String>) -> Result<Self, FillError> {
+		let placeholders: Vec<(String, String)> = var_matcher()
+			.captures_iter(&self)
+			.map(|m| m.extract())
+			.map(|(txt, [var])| (txt.to_string(), var.to_string()))
+			.collect();
+
+		// TODO can we avoid cloning all matches??? can't mutate `from` as captures_iter holds an
+		// immutable reference to original string
+
+		for (txt, var) in placeholders {
+			let value = env.get(&var).ok_or(FillError(var.to_string()))?;
+			self = self.replace(&txt, value);
+		}
+
+		Ok(self)
+	}
+}
+
+impl ReplaceableValue for toml::Table {
+	fn replace_in_self(self, env: &HashMap<String, String>) -> Result<Self, FillError> {
+		let mut new_map = toml::Table::default();
+		for (k, v) in self {
+			new_map.insert(
+				k.replace_in_self(env)?,
+				v.replace_in_self(env)?,
+			);
+		}
+
+		Ok(new_map)
+	}
+}
+
+impl ReplaceableValue for toml::Value {
+	fn replace_in_self(self, env: &HashMap<String, String>) -> Result<Self, FillError> {
+		match self {
+			toml::Value::String(x) => Ok(toml::Value::String(x.replace_in_self(env)?)),
+			toml::Value::Integer(x) => Ok(toml::Value::Integer(x)),
+			toml::Value::Float(x) => Ok(toml::Value::Float(x)),
+			toml::Value::Boolean(x) => Ok(toml::Value::Boolean(x)),
+			toml::Value::Datetime(x) => Ok(toml::Value::Datetime(x)),
+			toml::Value::Array(arr) => Ok(toml::Value::Array(arr.replace_in_self(env)?)),
+			toml::Value::Table(map) => Ok(toml::Value::Table(map.replace_in_self(env)?)),
+		}
+	}
+}
+
+impl<T: ReplaceableValue> ReplaceableValue for Vec<T> {
+	fn replace_in_self(self, env: &HashMap<String, String>) -> Result<Self, FillError> {
+		let mut new_arr = Vec::new();
+		for v in self {
+			new_arr.push(v.replace_in_self(env)?);
+		}
+		Ok(new_arr)
+	}
+}
+
+impl<T: ReplaceableValue> ReplaceableValue for StringOr<T> {
+	fn replace_in_self(self, env: &HashMap<String, String>) -> Result<Self, FillError> {
+		match self {
+			StringOr::Str(x) => Ok(StringOr::Str(x.replace_in_self(env)?)),
+			StringOr::T(x) => Ok(StringOr::T(x.replace_in_self(env)?)),
+		}
 	}
 }
